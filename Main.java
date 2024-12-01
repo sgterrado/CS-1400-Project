cae301ecd4b7d450e695f7d36427ac29cada1032
GooseJacket <73757413+GooseJacket@users.noreply.github.com>
2024-11-30 22:40:09 -0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,30 +1,35 @@
-public class Main {
+class Main {
+    public static final int GAD_QUESTIONS = 7;
+    public static final int DEP_QUESTIONS = 9;
+    public static final int NUM_QUESTIONS = 1 + GAD_QUESTIONS + DEP_QUESTIONS;
+
     public static void main(String[] args) {
-        //vars
-        final int NUM_QUESTIONS = 10;
-        final String TEXT_FILE = "wordFile.txt";
-        
-        //Read previous responses from file
-        //* open file
-        FileInputStream fileByteStream = new FileInputStream(TEXT_FILE); 
-        //add try/catch
-        Scanner inScnr = new Scanner(fileByteStream);  
-      
-        //* first line is num of lines following
-        int mainArrayLength = inScnr.nextInt(); //add try/catch
-      
-        //Add data to array
-        int[][] mainArray = new int[mainArrayLength][NUM_QUESTIONS + 1];
-      
-        int fileLineIndex = 0; int[] temp;
-        while (inScnr.hasNext()) {
-           temp = new int[NUM_QUESTIONS + 1]; //+1 because first one is date
-           for(int i = 0; i < NUM_QUESTIONS + 1; i++){
-             temp[i] = inScnr.nextInt(); //add try/catch
-           }
-           mainArray[fileLineIndex] = temp;
-           fileLineIndex++;
-        }
-        fileByteStream.close();
+        UI ui = new UI(); //UI houses the System.in scanner
+
+        //Read from File
+        ReadingWriting p = new ReadingWriting();
+        EntryList e = p.readF(ui);
+            //p.testPrint(); //Testing
+
+        //Ask Questions
+        Entry today = ui.takeSurvey(p.getGAD(), p.getDep());
+            //today.print(); //Testing
+        e.append(today);
+            //e.testingPrint(); //Testing
+
+        //Write to File
+        p.printF(e);
+
+        //Calculate Average
+        double[] averages = e.avgData(ui.getDate());
+
+        //Print Average
+        System.out.printf("Score average over the last 2 weeks: Anxiety = %.2f, ", averages[0]);
+        System.out.printf("Depression = %.2f\n", averages[1]);
+        ui.diagnose(averages);
+
+        //Give Suggestions
+        ui.respond();
+
     }
 }
